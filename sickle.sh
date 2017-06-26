7d5a92c2bc2e8d2cc069b84bc083e008883fc770
Anna Dahlgren <adahlgren@ucdavis.edu>
2017-06-26 11:13:02 -0700

    Create sickle.sh

---
+++ sickle.sh
@@ -0,0 +1,22 @@
+#load sickle
+module load sickle
+#load fastqc
+module load fastqc
+
+for filename in *R1_001.fastq.gz
+do
+#make base by removing R1_001.fastq.gz
+base=$(basename $filename .fastq.gz)
+echo $base
+#construct R2 filename
+base2=${base/R1/R2}
+echo $base2
+
+#run sickle
+sickle pe -f ${base}.fastq.gz -r ${base2}.fastq.gz -t sanger -o ${base}_sick.fastq -p ${base2}_sick.fastq -s ${base}_sick_singles.fastq
+
+#fastqc
+fastqc ${base}_sick.fastq
+fastqc ${base2}_sick.fastq
+
+done
