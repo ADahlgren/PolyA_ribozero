a1df80cb85c28e7e8d892b3a33c8c00c4e6f4739
Anna Dahlgren <adahlgren@ucdavis.edu>
2017-06-26 11:12:20 -0700

    Create trimmomatic.sh

---
+++ trimmomatic.sh
@@ -0,0 +1,22 @@
+#load trimmomatic
+module load trimmomatic
+#load fastqc
+module load fastqc
+
+for filename in *R1_001.fastq.gz
+do
+#make base by removing R1_001.fastq.gz
+base=$(basename $filename .fastq.gz)
+echo $base
+#construct R2 filename
+base2=${base/R1/R2}
+echo $base2
+
+#run trimmomatic
+trimmomatic PE ${base}.fastq.gz ${base2}.fastq.gz\  ${base}_trim.qc.fq ${base}_trim_s1_se ${base2}_trim.qc.fq ${base2}_trim_s2_se\ SLIDINGWINDOW:3:28
+
+#fastqc
+fastqc ${base}_trim.qc.fq
+fastqc ${base2}_trim.qc.fq
+
+done
