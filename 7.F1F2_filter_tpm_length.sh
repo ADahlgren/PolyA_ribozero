3812568c39d36e2da37f8369823d70cfee987df6
Anna Dahlgren <adahlgren@ucdavis.edu>
2018-03-20 16:07:55 -0700

    Fix nt length

--- 7.F1F2_filter_tpm_length.sh
+++ 7.F1F2_filter_tpm_length.sh
@@ -4,8 +4,8 @@ base=$(basename $filename _tab.bed)
 echo $base
 
 #filter out all transcripts with TPM < 0.1 and with length < 199
-#filter based on length - >=199 since lncRNA are longer than that size and a bunch of transcripts were 199 bp
+#filter based on length - >=200 based on definition of lncRNA (note: there were quite a few transcripts at 199bp)
 
-cat ${base}_tab.bed | awk '{if ($6 >= 0.1) {print $0}}' | awk '{if ($7 >= 199) {print $0}}' > ${base}_filter12.bed
+cat ${base}_tab.bed | awk '{if ($6 >= 0.1) {print $0}}' | awk '{if ($7 >= 200) {print $0}}' > ${base}_filter12.bed
 
 done
