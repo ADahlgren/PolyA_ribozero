3ae052caa3af5619bba4dc3d7a514dacd42af17b
Anna Dahlgren <adahlgren@ucdavis.edu>
2018-06-02 19:32:58 -0700

    fix note

--- 7.F1F2_filter_tpm_length.sh
+++ 7.F1F2_filter_tpm_length.sh
@@ -3,7 +3,7 @@ do
 base=$(basename $filename _tab.bed)
 echo $base
 
-#filter out all transcripts with TPM < 0.1 and with length < 199
+#filter out all transcripts with TPM < 0.1 and with length < 200
 #filter based on length - >=200 based on definition of lncRNA (note: there were quite a few transcripts at 199bp)
 
 cat ${base}_tab.bed | awk '{if ($6 >= 0.1) {print $0}}' | awk '{if ($7 >= 200) {print $0}}' > ${base}_filter12.bed
