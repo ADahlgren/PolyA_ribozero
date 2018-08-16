d553a67af560959a1ba8f8730ba9fac85443d06c
Anna Dahlgren <adahlgren@ucdavis.edu>
2018-08-16 09:32:52 -0700

    change TPM filter based on histogram

--- 7.F1F2_filter_tpm_length.sh
+++ 7.F1F2_filter_tpm_length.sh
@@ -3,9 +3,9 @@ do
 base=$(basename $filename _tab.bed)
 echo $base
 
-#filter out all transcripts with TPM < 0.1 and with length < 200
+#filter out all transcripts with TPM < 0.6 (based on histogram of TPM frequencies)
 #filter based on length - >=200 based on definition of lncRNA (note: there were quite a few transcripts at 199bp)
 
-cat ${base}_tab.bed | awk '{if ($6 >= 0.1) {print $0}}' | awk '{if ($7 >= 200) {print $0}}' > ${base}_filter12.bed
+cat ${base}_tab.bed | awk '{if ($6 >= 0.6) {print $0}}' | awk '{if ($7 >= 200) {print $0}}' > ${base}_filter12.bed
 
 done
