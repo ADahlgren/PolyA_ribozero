b9bff025a2fba2ac61dd0d1c8211e65aa8e0214e
Anna Dahlgren <adahlgren@ucdavis.edu>
2018-11-25 10:00:17 -0800

    update for equcab3

--- 7.F1F2_filter_tpm_length.sh
+++ 7.F1F2_filter_tpm_length.sh
@@ -1,11 +1,10 @@
-for filename in *tab.bed
+for filename in *length.bed
 do
-base=$(basename $filename _tab.bed)
+base=$(basename $filename _length.bed)
 echo $base
 
-#filter out all transcripts with TPM < 0.6 (based on histogram of TPM frequencies)
-#filter based on length - >=200 based on definition of lncRNA (note: there were quite a few transcripts at 199bp)
-
-cat ${base}_tab.bed | awk '{if ($6 >= 0.6) {print $0}}' | awk '{if ($7 >= 200) {print $0}}' > ${base}_filter12.bed
+#filter out all transcripts with TPM < 0.5 (based on histogram) and with length < 200 (based on definition of lncRNA
+#though there are quite a few at 199 bp
+cat ${base}_length.bed | awk '{if ($6 >= 0.5) {print $0}}' | awk '{if ($7 >= 200) {print $0}}' > ${base}_filter12.bed
 
 done
