6cc14d322e92cfb3e9642554798fde48c4080b50
Anna Dahlgren <adahlgren@ucdavis.edu>
2019-09-18 17:56:11 -0700

    update to equcab3

--- 4.star_pass2.sh
+++ 4.star_pass2.sh
@@ -4,9 +4,9 @@ module load star/2.5.2b
 
 mkdir pass2
 
-for filename in 683610_Liver*R1_001_trim.qc.fq
+for filename in *R1_001_sick.fastq
 do
-base=$(basename $filename .qc.fq)
+base=$(basename $filename .fastq)
 echo $base
 base2=${base/R1/R2}
 echo $base2
@@ -20,10 +20,15 @@ echo $base2
   #outFileNamePrefix - location and name of output
   #outSAMstrandField intronMotif - for XS strand attribute needed for stringtie
   #outSAMtype - type of output; sorted bam file (so don't have to samtools sort later)
-STAR --runThreadN 12 --genomeDir star_index \
---sjdbFileChrStartEnd /share/finnolab/adahl/polyA_ribozero/mapping/pass1/683610_Liver_Ribozero_S51_R1_001_trim_SJ.out.tab /share/finnolab/adahl/polyA_ribozero/mapping/pass1/683610_Liver_S61_R1_001_trim_SJ.out.tab \
---readFilesIn /share/finnolab/adahl/polyA_ribozero/mapping/${base}.qc.fq \
-/share/finnolab/adahl/polyA_ribozero/mapping/${base2}.qc.fq \
+STAR --runThreadN 10 --genomeDir /share/finnolab/reference/equCab3.0/NCBI/indcies/star_index \
+--sjdbFileChrStartEnd pass1/683610_Liver_Ribozero_S51_R1_001_sick_SJ.out.tab \
+pass1/683610_Parietal_Cortex_S55_R1_001_sick_SJ.out.tab \
+pass1/686521_Parietal_Cortex_Ribozero_S52_R1_001_sick_SJ.out.tab \
+pass1/683610_Liver_S61_R1_001_sick_SJ.out.tab \
+pass1/686521_Liver_Ribozero_S50_R1_001_sick_SJ.out.tab \
+pass1/686521_Parietal_Cortex_S54_R1_001_sick_SJ.out.tab \
+pass1/683610_Parietal_Cortex_Ribozero_S53_R1_001_sick_SJ.out.tab \
+pass1/686521_Liver_S60_R1_001_sick_SJ.out.tab \
+--readFilesIn ${base}.fastq ${base2}.fastq \
 --outFileNamePrefix pass2/${base}_ --outSAMstrandField intronMotif --outSAMtype BAM SortedByCoordinate
-
 done
