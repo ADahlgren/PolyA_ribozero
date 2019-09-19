737d60c0e71b5a62b709a878f7d7e20d7521bb90
Anna Dahlgren <adahlgren@ucdavis.edu>
2019-09-18 17:44:05 -0700

    update to remove ballgown

--- 5.stringtie.sh
+++ 5.stringtie.sh
@@ -3,27 +3,19 @@
 module load stringtie/1.3.2d
 
 #link mapped bam files from star pass2 and gtf files from mapping file
-ln -fs /share/finnolab/adahl/polyA_ribozero/mapping/pass2/*.out.bam
-ln -fs /share/finnolab/adahl/polyA_ribozero/mapping/star_index/*.GTF
 
 for filename in *.bam
 do
 base=$(basename $filename .out.bam)
 echo $base
 
-#already make ballgown directory and then this is to make subdirectories separating ballgown files for each sample
-#may use these files later to look at differential expression?
-cd ballgowns
-mkdir ${base}_ballgown
-cd ../
 
 #stringtie
 #G - GTF file (used both merged and filtered, proceded with filtered to lnc analysis)
 #o - output gtf name
 #A - report gene abundances in abund.out file
-#b - path to where ballgown files should be outputed 
+ 
 echo "stringtie time"
-stringtie ${base}.out.bam -G filtered4_refined_Alltissues.GTF -o ${base}_refined_stringtie.gtf -A ${base}_refined_abund.out \
--b /share/finnolab/adahl/polyA_ribozero/ballgown/refined2/ballgowns/${base}_ballgown
+stringtie ${base}.out.bam -G ref_EquCab3.0_top_level.gtf.GeneBank -o ${base}_equcab3_stringtie.gtf -A ${base}_equcab3_abund.out
 
 done
