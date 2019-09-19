f0aab00ec4e0078de7619c739cc83c8c231a7f6f
Anna Dahlgren <adahlgren@ucdavis.edu>
2019-09-18 17:54:43 -0700

    update to equcab3

--- 3.star_pass1.sh
+++ 3.star_pass1.sh
@@ -3,9 +3,9 @@ module load star/2.5.2b
 mkdir pass1
 
 #make base (this is just subset for now)
-for filename in 683610_Liver*R1_001_trim.qc.fq
+for filename in *R1_001_*.fastq
 do
-base=$(basename $filename .qc.fq)
+base=$(basename $filename .fastq)
 echo $base
 base2=${base/R1/R2}
 echo $base2
@@ -17,10 +17,9 @@ echo $base2
   #outSAMstrandField intronMotif - for XS strand attribute needed for stringtie
   #outSAMtype - type of output; unsorted bam file (will sort during second pass)
 
-STAR --runThreadN 12 --genomeDir star_index \
---readFilesIn /share/finnolab/adahl/polyA_ribozero/mapping/${base}.qc.fq \
-/share/finnolab/adahl/polyA_ribozero/mapping/${base2}.qc.fq \
---outFileNamePrefix pass1/${base}_ --outSAMstrandField intronMotif --outSAMtype BAM Unsorted
+STAR --runThreadN 10 --genomeDir /share/finnolab/reference/equCab3.0/NCBI/indcies/star_index \
+--readFilesIn ${base}.fastq ${base2}.fastq \
+--outFileNamePrefix pass1/${base}_ --outSAMstrandField intronMotif --outSAMtype BAM SortedByCoordinate
 done
 
 #copy .tab files into pass2
